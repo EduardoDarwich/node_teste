e0ded78a3b4d6056a26d6d14483fd3c49aab231b
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-05-12 09:30:01 -0300

    feat: add new build

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -2,13 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("../controllers");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.post('/', (req, res) => {
-    console.log(req.body);
-    return res.status(http_status_codes_1.StatusCodes.ACCEPTED).json(req.body);
-});
-router.post('/', (req, res) => {
+router.post('/cidades', controllers_1.CidadesController.createBodyValidator, controllers_1.CidadesController.create, controllers_1.CidadesController.createValidation);
+router.get('/', (req, res) => {
     return res.send('aoba');
 });
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,12 +1,12 @@
 import {Router} from 'express';
 import {StatusCodes} from 'http-status-codes';
 import { CidadesController } from '../controllers';
-import { createBodyValidator, createQueryValidator } from '../controllers/cidades/Create';
+
 
 
 const router = Router();
 
-router.post('/cidades', CidadesController.createBodyValidator, CidadesController.create, CidadesController.createQueryValidator);
+router.post('/cidades', CidadesController.createBodyValidator, CidadesController.create, CidadesController.createValidation);
 
 router.get('/', (req, res) => {
     return res.send('aoba')
